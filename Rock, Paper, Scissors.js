82429adb39c1bfa78932a3f2e27fd5c7a0ee564f
GordonQuach <qgordon8@vt.edu>
2016-07-21 02:40:53 -0400

    Update Rock, Paper, Scissors.js

--- Rock, Paper, Scissors.js
+++ Rock, Paper, Scissors.js
@@ -12,30 +12,30 @@ if (computerChoice < 0.34) {
 
 var compare = function (choice1, choice2) {
     if (choice1 === choice2) {
-        return ("The result is a tie!");
+        document.write ("The result is a tie!");
     }
     else if (choice1 === "rock") {
         if (choice2 === "scissors") {
-            return ("Rock wins!");
+            document.write ("Rock wins!");
         }
         else {
-            return ("Paper wins!");
+            document.write ("Paper wins!");
         }
     }
     else if (choice1 === "paper") {
         if (choice2 === "rock") {
-            return ("Paper wins!");
+            document.write ("Paper wins!");
         }
         else {
-            return ("Scissors wins!");
+            document.write ("Scissors wins!");
         }
     }
     else if (choice1 === "scissors") {
         if (choice2 === "rock") {
-            return ("Rock wins!");
+            document.write ("Rock wins!");
         }
         else {
-            return ("Scissors wins!");
+            document.write ("Scissors wins!");
         }
     }
 };
