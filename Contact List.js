57bdb0432ada737d584dbb3012734e5add90726d
GordonQuach <qgordon8@vt.edu>
2016-08-03 18:13:45 -0400

    Update Contact List.js

--- Contact List.js
+++ Contact List.js
@@ -1,3 +1,5 @@
+// This was a project done to learn how to build contact lists and store/output information. //
+
 var friends = {
     bill: {
         firstName: "Bill",
@@ -13,11 +15,16 @@ var friends = {
     }
 };
 
+
+// This prints out all the entries we have in our friends object. // 
+
 var list = function (friends) {
-    for (var contacts in friends) {
-        document.write(contacts);
+    for (var key in friends) {
+        document.write(key);
     }
 };
+
+//  This takes a single argument, name. If the name matches any of the first names in friends, it should log that friend's contact information and return it. //
 
 var search = function (name) {
     for (var key in friends) {
